9a3055c78fc026b41e9c7e7df871eb2456ce6e4f
Aeron Jead D. Marquez <marquezaeronjead@gmail.com>
2026-05-10 01:01:54 +0800

    Delete libs directory

--- libs/load.php
+++
@@ -1,5 +0,0 @@
-<?php
-$dir = "../data/";
-if (!file_exists($dir)) mkdir($dir, 0777, true);
-$files = array_diff(scandir($dir), array('.', '..'));
-echo json_encode(array_values($files));
--- libs/save.php
+++
@@ -1,5 +0,0 @@
-<?php
-$data = file_get_contents("php://input");
-file_put_contents("../data/students.json", $data);
-echo json_encode(["status" => "saved"]);
-?>
